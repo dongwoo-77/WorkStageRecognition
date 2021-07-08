f205dd94f93f5c713c4f2549e3d5a63c3bd25231
dongwoo-77 <77ndw77@gmail.com>
2021-07-08 17:18:26 +0900

    grad cam

---
+++ grad_cam.py
@@ -0,0 +1,406 @@
+import cv2
+import numpy as np
+import torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import ttach as tta
+import torchvision
+import torchvision.models as models
+import os
+from torchvision.transforms import Compose, Normalize, ToTensor
+import argparse
+from torch.autograd import Variable
+from torch.autograd import Function
+
+
+class GuidedBackpropReLU(Function):
+    @staticmethod
+    def forward(self, input_img):
+        positive_mask = (input_img > 0).type_as(input_img)
+        output = torch.addcmul(torch.zeros(input_img.size()).type_as(input_img), input_img, positive_mask)
+        self.save_for_backward(input_img, output)
+        return output
+
+    @staticmethod
+    def backward(self, grad_output):
+        input_img, output = self.saved_tensors
+        grad_input = None
+
+        positive_mask_1 = (input_img > 0).type_as(grad_output)
+        positive_mask_2 = (grad_output > 0).type_as(grad_output)
+        grad_input = torch.addcmul(torch.zeros(input_img.size()).type_as(input_img),
+                                   torch.addcmul(torch.zeros(input_img.size()).type_as(input_img), grad_output,
+                                                 positive_mask_1), positive_mask_2)
+        return grad_input
+
+
+def get_2d_projection(activation_batch):
+    # TBD: use pytorch batch svd implementation
+    projections = []
+    for activations in activation_batch:
+        reshaped_activations = (activations).reshape(activations.shape[0], -1).transpose()
+        # Centering before the SVD seems to be important here,
+        # Otherwise the image returned is negative
+        reshaped_activations = reshaped_activations - reshaped_activations.mean(axis=0)
+        U, S, VT = np.linalg.svd(reshaped_activations, full_matrices=True)    
+        projection = reshaped_activations @ VT[0, :]
+        projection = projection.reshape(activations.shape[1 : ])
+        projections.append(projection)
+    return np.float32(projections)
+
+
+class ActivationsAndGradients:
+    """ Class for extracting activations and
+    registering gradients from targetted intermediate layers """
+
+    def __init__(self, model, target_layer, reshape_transform):
+        self.model = model
+        self.gradients = []
+        self.activations = []
+        self.reshape_transform = reshape_transform
+
+        target_layer.register_forward_hook(self.save_activation)
+        target_layer.register_backward_hook(self.save_gradient)
+
+    def save_activation(self, module, input, output):
+        activation = output
+        if self.reshape_transform is not None:
+            activation = self.reshape_transform(activation)
+        self.activations.append(activation.cpu().detach())
+
+    def save_gradient(self, module, grad_input, grad_output):
+        # Gradients are computed in reverse order
+        grad = grad_output[0]
+        if self.reshape_transform is not None:
+            grad = self.reshape_transform(grad)
+        self.gradients = [grad.cpu().detach()] + self.gradients
+
+    def __call__(self, x):
+        self.gradients = []
+        self.activations = []        
+        return self.model(x)
+
+
+
+class BaseCAM:
+    def __init__(self, 
+                 model, 
+                 target_layer,
+                 use_cuda=False,
+                 reshape_transform=None):
+        self.model = model.eval()
+        self.target_layer = target_layer
+        self.cuda = use_cuda
+        if self.cuda:
+            self.model = model.cuda()
+        self.reshape_transform = reshape_transform
+        self.activations_and_grads = ActivationsAndGradients(self.model, 
+            target_layer, reshape_transform)
+
+    def forward(self, input_img):
+        return self.model(input_img)
+
+    def get_cam_weights(self,
+                        input_tensor,
+                        target_category,
+                        activations,
+                        grads):
+        raise Exception("Not Implemented")
+
+    def get_loss(self, output, target_category):
+        loss = 0
+        for i in range(len(target_category)):
+            loss = loss + output[i, target_category[i]]
+        return loss
+
+    def get_cam_image(self,
+                      input_tensor,
+                      target_category,
+                      activations,
+                      grads,
+                      eigen_smooth=False):
+        weights = self.get_cam_weights(input_tensor, target_category, activations, grads)
+        weighted_activations = weights[:, :, None, None] * activations
+        if eigen_smooth:
+            cam = get_2d_projection(weighted_activations)
+        else:
+            cam = weighted_activations.sum(axis=1)
+        return cam
+
+    def forward(self, input_tensor, target_category=None, eigen_smooth=False):
+
+        if self.cuda:
+            input_tensor = input_tensor.cuda()
+
+        output = self.activations_and_grads(input_tensor)
+
+        if type(target_category) is int:
+            target_category = [target_category] * input_tensor.size(0)
+
+        if target_category is None:
+            target_category = np.argmax(output.cpu().data.numpy(), axis=-1)
+        else:
+            assert(len(target_category) == input_tensor.size(0))
+
+        self.model.zero_grad()
+        loss = self.get_loss(output, target_category)
+        loss.backward(retain_graph=True)
+
+        activations = self.activations_and_grads.activations[-1].cpu().data.numpy()
+        grads = self.activations_and_grads.gradients[-1].cpu().data.numpy()
+
+        cam = self.get_cam_image(input_tensor, target_category, 
+            activations, grads, eigen_smooth)
+
+        cam = np.maximum(cam, 0)
+
+        result = []
+        for img in cam:
+            img = cv2.resize(img, input_tensor.shape[-2:][::-1])
+            img = img - np.min(img)
+            img = img / np.max(img)
+            result.append(img)
+        result = np.float32(result)
+        return result
+
+    def forward_augmentation_smoothing(self,
+                                       input_tensor,
+                                       target_category=None,
+                                       eigen_smooth=False):
+        transforms = tta.Compose(
+            [
+                tta.HorizontalFlip(),
+                tta.Multiply(factors=[0.9, 1, 1.1]),
+            ]
+        )
+        cams = []
+        for transform in transforms:
+            augmented_tensor = transform.augment_image(input_tensor)
+            cam = self.forward(augmented_tensor,
+                target_category, eigen_smooth)
+
+            # The ttach library expects a tensor of size BxCxHxW
+            cam = cam[:, None, :, :]
+            cam = torch.from_numpy(cam)
+            cam = transform.deaugment_mask(cam)
+
+            # Back to numpy float32, HxW
+            cam = cam.numpy()
+            cam = cam[:, 0, :, :]
+            cams.append(cam)
+
+        cam = np.mean(np.float32(cams), axis=0)
+        return cam
+
+    def __call__(self,
+                 input_tensor,
+                 target_category=None,
+                 aug_smooth=False,
+                 eigen_smooth=False):
+        if aug_smooth is True:
+            return self.forward_augmentation_smoothing(input_tensor,
+                target_category, eigen_smooth)
+
+        return self.forward(input_tensor,
+            target_category, eigen_smooth)
+
+
+class GradCAM(BaseCAM):
+    def __init__(self, model, target_layer, use_cuda=False, 
+        reshape_transform=None):
+        super(GradCAM, self).__init__(model, target_layer, use_cuda, reshape_transform)
+
+    def get_cam_weights(self,
+                        input_tensor,
+                        target_category,
+                        activations,
+                        grads):
+        return np.mean(grads, axis=(2, 3))
+
+
+class GuidedBackpropReLUModel:
+    def __init__(self, model, use_cuda):
+        self.model = model
+        self.model.eval()
+        self.cuda = use_cuda
+        if self.cuda:
+            self.model = self.model.cuda()
+
+    def forward(self, input_img):
+        return self.model(input_img)
+
+    def recursive_replace_relu_with_guidedrelu(self, module_top):
+        for idx, module in module_top._modules.items():
+            self.recursive_replace_relu_with_guidedrelu(module)
+            if module.__class__.__name__ == 'ReLU':
+                module_top._modules[idx] = GuidedBackpropReLU.apply
+
+    def recursive_replace_guidedrelu_with_relu(self, module_top):
+        try:
+            for idx, module in module_top._modules.items():
+                self.recursive_replace_guidedrelu_with_relu(module)
+                if module == GuidedBackpropReLU.apply:
+                    module_top._modules[idx] = torch.nn.ReLU()
+        except:
+            pass
+
+
+    def __call__(self, input_img, target_category=None):
+        # replace ReLU with GuidedBackpropReLU
+        self.recursive_replace_relu_with_guidedrelu(self.model)
+
+        if self.cuda:
+            input_img = input_img.cuda()
+
+        input_img = input_img.requires_grad_(True)
+
+        output = self.forward(input_img)
+
+        if target_category is None:
+            target_category = np.argmax(output.cpu().data.numpy())
+
+        loss = output[0, target_category]
+        loss.backward(retain_graph=True)
+
+        output = input_img.grad.cpu().data.numpy()
+        output = output[0, :, :, :]
+        output = output.transpose((1, 2, 0))
+
+        # replace GuidedBackpropReLU back with ReLU
+        self.recursive_replace_guidedrelu_with_relu(self.model)
+
+        return output
+
+
+def show_cam_on_image(img: np.ndarray,
+                      mask: np.ndarray,
+                      use_rgb: bool = False,
+                      colormap: int = cv2.COLORMAP_JET) -> np.ndarray:
+    """ This function overlays the cam mask on the image as an heatmap.
+    By default the heatmap is in BGR format.
+    :param img: The base image in RGB or BGR format.
+    :param mask: The cam mask.
+    :param use_rgb: Whether to use an RGB or BGR heatmap, this should be set to True if 'img' is in RGB format.
+    :param colormap: The OpenCV colormap to be used.
+    :returns: The default image with the cam overlay.
+    """
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), colormap)
+    if use_rgb:
+        heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
+    heatmap = np.float32(heatmap) / 255
+
+    if np.max(img) > 1:
+        raise Exception("The input image should np.float32 in the range [0, 1]")
+
+    cam = heatmap + img
+    cam = cam / np.max(cam)
+    return np.uint8(255 * cam)
+
+
+def deprocess_image(img):
+    """ see https://github.com/jacobgil/keras-grad-cam/blob/master/grad-cam.py#L65 """
+    img = img - np.mean(img)
+    img = img / (np.std(img) + 1e-5)
+    img = img * 0.1
+    img = img + 0.5
+    img = np.clip(img, 0, 1)
+    return np.uint8(img * 255)
+
+
+def preprocess_image(img: np.ndarray, mean=None, std=None) -> torch.Tensor:
+    if std is None:
+        std = [0.5, 0.5, 0.5]
+    if mean is None:
+        mean = [0.5, 0.5, 0.5]
+
+    preprocessing = Compose([
+        ToTensor(),
+        Normalize(mean=mean, std=std)
+    ])
+
+    return preprocessing(img.copy()).unsqueeze(0)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--aug_smooth', action='store_true',
+                        help='Apply test time augmentation to smooth the CAM')
+    parser.add_argument('--eigen_smooth', action='store_true',
+                        help='Reduce noise by taking the first principle componenet'
+                        'of cam_weights*activations')
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    """ python cam.py -image-path <path_to_image>
+    Example usage of loading an image, and computing:
+        1. CAM
+        2. Guided Back Propagation
+        3. Combining both
+    """
+
+    args = get_args()
+
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+    model = models.resnext50_32x4d()
+    model.fc = nn.Linear(in_features=2048, out_features=7, bias=True)
+    model.load_state_dict(torch.load('/home/nam/workspace/WorkStageRecognition/model/model_state_dict_1', map_location=device))
+    model.to(device)
+    model.eval()
+
+    # Choose the target layer you want to compute the visualization for.
+    # Usually this will be the last convolutional layer in the model.
+    # Some common choices can be:
+    # Resnet18 and 50: model.layer4[-1]
+    # VGG, densenet161: model.features[-1]
+    # mnasnet1_0: model.layers[-1]
+    # You can print the model to help chose the layer
+    target_layer = model.layer4[-1]
+    # print(target_layer)
+
+    cam = GradCAM(model=model,
+                    target_layer=target_layer,
+                    use_cuda=device)
+
+    for i in range(100):
+        rgb_img = cv2.imread('/home/nam/workspace/WorkStageRecognition/images/test/7/{}.png'.format(i), 1)[:, :, ::-1]
+        rgb_img = np.float32(rgb_img) / 255
+        input_tensor = preprocess_image(rgb_img, mean=[0.485, 0.456, 0.406], 
+                                                std=[0.229, 0.224, 0.225])
+
+        # If None, returns the map for the highest scoring category.
+        # Otherwise, targets the requested category.
+        target_category = None
+
+        # AblationCAM and ScoreCAM have batched implementations.
+        # You can override the internal batch size for faster computation.
+        cam.batch_size = 32
+
+        grayscale_cam = cam(input_tensor=input_tensor,
+                            target_category=target_category,
+                            aug_smooth=args.aug_smooth,
+                            eigen_smooth=args.eigen_smooth)
+
+        # Here grayscale_cam has only one image in the batch
+        grayscale_cam = grayscale_cam[0, :]
+
+        cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+
+        # cam_image is RGB encoded whereas "cv2.imwrite" requires BGR encoding.
+        cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
+
+        gb_model = GuidedBackpropReLUModel(model=model, use_cuda=device)
+        gb = gb_model(input_tensor, target_category=target_category)
+
+        cam_mask = cv2.merge([grayscale_cam, grayscale_cam, grayscale_cam])
+        cam_gb = deprocess_image(cam_mask * gb)
+        gb = deprocess_image(gb)
+
+        cv2.imwrite('/home/nam/workspace/WorkStageRecognition/result/grad_cam/cam_image/7/{}.png'.format(i), cam_image)
+        # cv2.imwrite('/home/nam/workspace/WorkStageRecognition/result/grad_cam/cam_gb/1/{}.png'.format(i), gb)
+        # cv2.imwrite('/home/nam/workspace/WorkStageRecognition/result/grad_cam/cam_gb/1/{}.png'.format(i), cam_gb)
